1edb5f9aa1c3c075bc06f661849f89ba1933c99d
Kyle Ryan Saelee <tfbk@wetigers.org>
2019-03-20 08:01:49 -0700

    Update World Map OOP - Kyle Ryan Saelee.py

--- notes/World Map OOP - Kyle Ryan Saelee.py
+++ notes/World Map OOP - Kyle Ryan Saelee.py
@@ -321,7 +321,7 @@ assault_rifle = Gun("Diamond Assault Rifle", 500, 10000)
 
 # Characters
 c2 = Character("Kid2", 100, Iron_Sword, None)
-c1 = Character("Kid1", 100, Diamond_Sword, None)
+c1 = Character("Kid1", 100, Diamond_Sword, No
 c1.attack(c2)
 
 DINING_ROOM = Room("Dining Room", "LIVING_ROOM", "MASTER_BEDROOM",
