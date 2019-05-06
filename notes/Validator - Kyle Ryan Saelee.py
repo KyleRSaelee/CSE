9c0c654029994e0750f0ab638ff5f1987989cc07
KyleRSaelee <42383656+KyleRSaelee@users.noreply.github.com>
2019-05-06 08:44:37 -0700

    save

--- notes/Validator - Kyle Ryan Saelee.py
+++ notes/Validator - Kyle Ryan Saelee.py
@@ -1,24 +1,38 @@
 import csv
-
+test_num = "4556737586899855"
 
 def reverse(num: str):
-    print(num)
-    print(num[::-1])
+    return num[::-1]
 
 
 def remove_last_num(num: str):
-    print(num)
     remaining_nums = num[0:15]
     print(remaining_nums)
+    return True
 
+def validate(number: str):
+    last_num = int(number[15])
+    remaining_nums = number[0:15]
+    print(remaining_nums)
 
-with open("Book1.csv", 'r') as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        reader = csv.reader(old_csv)
-        writer = csv.writer(new_csv)
-        print("Processing...")
-        for row in reader:
-            credit_card_num = row[0]
-            remove_last_num(credit_card_num)
+    reversed_num = list(reverse(remaining_nums))
+    print(reversed_num)
 
-reverse("8721389808186650")
+    for index in range(len(reversed_num)):
+        reversed_num[index] = int(reversed_num[index])
+        if reversed_num[index] % 2 == 0:
+
+    print(reversed_num)
+
+print(validate(test_num))
+
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#         for row in reader:
+#             num = row[0]
+#             if validate(num):
+#                 writer.writerow(row)
+#         print("OK")
