a1c4bc14a61e36df4e07c7a34df2a149a3fb861c
KyleRSaelee <42383656+KyleRSaelee@users.noreply.github.com>
2019-05-07 08:45:56 -0700

    save

--- notes/Validator - Kyle Ryan Saelee.py
+++ notes/Validator - Kyle Ryan Saelee.py
@@ -26,13 +26,13 @@ def validate(number: str):
 
 print(validate(test_num))
 
-# with open("Book1.csv", 'r') as old_csv:
-#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-#         reader = csv.reader(old_csv)
-#         writer = csv.writer(new_csv)
-#         print("Processing...")
-#         for row in reader:
-#             num = row[0]
-#             if validate(num):
-#                 writer.writerow(row)
-#         print("OK")
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+     reader = csv.reader(old_csv)
+    writer = csv.writer(new_csv)
+    print("Processing...")
+    for row in reader:
+        num = row[0]
+        if validate(num):
+               writer.writerow(row)
+        print("OK")
